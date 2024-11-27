2b53e54824e02f5186864fad5b2602bb361f2893
20-shimizu <reborn.titech@gmail.com>
2024-11-27 15:47:08 +0900

    隣接行列取得の追加

--- prepare.py
+++ prepare.py
@@ -15,31 +15,66 @@ inside_outside = image[:, :, 3]  # 4チャンネル目（内外判定）
 
 # 部屋番号のユニーク値を取得（255や0は無視）
 unique_rooms = np.unique(room_number[(room_number > 0) & (inside_outside == 255)])
-
-# print(f"unique_rooms : {unique_rooms}")
+n_rooms = len(unique_rooms)
 
 # 情報を保存するリスト
-room_data = []
+input_data = []
+output_data = []
+
+# 隣接行列
+door_adjacency_matrix = np.zeros((n_rooms, n_rooms), dtype=int)
+wall_adjacency_matrix = np.zeros((n_rooms, n_rooms), dtype=int)
+spatial_adjacency_matrix = np.zeros((n_rooms, n_rooms), dtype=int)
+
+# ドア、窓のマスク
+wall_mask = (room_type == 16) & (inside_outside == 255)
+door_mask = (room_type == 17) & (inside_outside == 255)
+
+# 膨張処理時のカーネル
+kernel = np.array([[0,0,0,1,0,0,0],
+                   [0,0,0,1,0,0,0],
+                   [0,0,0,1,0,0,0],
+                   [1,1,1,1,1,1,1],
+                   [0,0,0,1,0,0,0],
+                   [0,0,0,1,0,0,0],
+                   [0,0,0,1,0,0,0]], dtype=np.uint8)
 
 # 各部屋ごとに情報を抽出
 for room_id in unique_rooms:
     mask = room_number == room_id  # 部屋のマスク
     room_area = np.sum(mask)  # 面積（ピクセル数）
     room_type_id = np.unique(room_type[mask])[0]  # 部屋タイプ（ユニークなので1つ）
-    
     # 周囲の長さを計算
     contour = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
     perimeter = cv2.arcLength(contour[0], True) if contour else 0
-    
     # 重心を計算
     y_center, x_center = measurements.center_of_mass(mask)
-    
     # # ドアの接触部分の計算
     # door_mask = (structure == 255) & (inside_outside == 255)  # ドアのマスク
     # adjacent_doors = np.sum(cv2.dilate(mask.astype(np.uint8), np.ones((3, 3)), iterations=1) & door_mask)
-    
+
+    # 膨張範囲
+    expanded_mask = cv2.dilate(mask.astype(np.uint8), kernel, iterations=1)
+    for room_id_other in unique_rooms:
+        if room_id >= room_id_other:
+            continue
+        mask_other = room_number == room_id_other
+        # 相手側の膨張範囲
+        expanded_mask_other = cv2.dilate(mask_other.astype(np.uint8), kernel, iterations=1)
+
+        if np.any(expanded_mask & expanded_mask_other & door_mask):
+            door_adjacency_matrix[room_id-1, room_id_other-1] = 1
+            door_adjacency_matrix[room_id_other-1, room_id-1] = 1
+            spatial_adjacency_matrix[room_id-1, room_id_other-1] = 1
+            spatial_adjacency_matrix[room_id_other-1, room_id-1] = 1
+        elif np.any(expanded_mask & expanded_mask_other & wall_mask):
+            wall_adjacency_matrix[room_id-1, room_id_other-1] = 1
+            wall_adjacency_matrix[room_id_other-1, room_id-1] = 1
+            spatial_adjacency_matrix[room_id-1, room_id_other-1] = 1
+            spatial_adjacency_matrix[room_id_other-1, room_id-1] = 1
+
     # 情報を保存
-    room_data.append({
+    input_data.append({
         "Room ID": room_id,
         "Room Type": room_type_id,
         # "Adjacent Doors": adjacent_doors,
@@ -49,8 +84,16 @@ for room_id in unique_rooms:
         "Centroid Y": y_center,
     })
 
-# データフレームに変換してCSV出力
-df = pd.DataFrame(room_data)
-df.to_csv(f"data/floorplan/input_data/{image_name}.csv", index=False)
+output_data.append({
+    "Door Adjacency": door_adjacency_matrix,
+    "Wall Adjacency": wall_adjacency_matrix,
+    "Spatial Adjacency": spatial_adjacency_matrix,
+})
 
-print("部屋ごとの情報をCSVに保存しました: room_data.csv")
+# データフレームに変換してCSV出力
+df_input = pd.DataFrame(input_data)
+df_output = pd.DataFrame(output_data)
+df_input.to_csv(f"data/floorplan/input_data/{image_name}.csv", index=False)
+df_output.to_csv(f"data/floorplan/output_data/{image_name}.csv", index=False)
+
+print(f"{image_name}.pngの情報をCSVに保存しました")
